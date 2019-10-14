10659af99a368cf7ea5e8e6ea606516d0eaf5d64
Aiden-p77 <56548726+Aiden-p77@users.noreply.github.com>
2019-10-14 18:27:56 +0330

    Add files via upload

---
+++ e_Cos_Arctan/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace e_Cos_Arctan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.tbLength = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lExp = new System.Windows.Forms.Label();
+            this.lArctan = new System.Windows.Forms.Label();
+            this.lCos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Upper :";
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(55, 17);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(58, 20);
+            this.tbX.TabIndex = 2;
+            this.tbX.TextChanged += new System.EventHandler(this.tbX_TextChanged);
+            // 
+            // tbLength
+            // 
+            this.tbLength.Location = new System.Drawing.Point(192, 17);
+            this.tbLength.Name = "tbLength";
+            this.tbLength.Size = new System.Drawing.Size(58, 20);
+            this.tbLength.TabIndex = 3;
+            this.tbLength.TextChanged += new System.EventHandler(this.tbLength_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "e^x :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Arctan :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Cos(x) :";
+            // 
+            // lExp
+            // 
+            this.lExp.AutoSize = true;
+            this.lExp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lExp.Location = new System.Drawing.Point(72, 53);
+            this.lExp.Name = "lExp";
+            this.lExp.Size = new System.Drawing.Size(0, 13);
+            this.lExp.TabIndex = 7;
+            // 
+            // lArctan
+            // 
+            this.lArctan.AutoSize = true;
+            this.lArctan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lArctan.Location = new System.Drawing.Point(72, 76);
+            this.lArctan.Name = "lArctan";
+            this.lArctan.Size = new System.Drawing.Size(0, 13);
+            this.lArctan.TabIndex = 8;
+            // 
+            // lCos
+            // 
+            this.lCos.AutoSize = true;
+            this.lCos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lCos.Location = new System.Drawing.Point(72, 98);
+            this.lCos.Name = "lCos";
+            this.lCos.Size = new System.Drawing.Size(0, 13);
+            this.lCos.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 118);
+            this.Controls.Add(this.lCos);
+            this.Controls.Add(this.lArctan);
+            this.Controls.Add(this.lExp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbLength);
+            this.Controls.Add(this.tbX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.TextBox tbLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lExp;
+        private System.Windows.Forms.Label lArctan;
+        private System.Windows.Forms.Label lCos;
+    }
+}
+
---
+++ e_Cos_Arctan/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace e_Cos_Arctan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbX_TextChanged(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void tbLength_TextChanged(object sender, EventArgs e)
+        {
+            if (tbX.Text == "" || tbLength.Text == "")
+            {
+                MessageBox.Show("Error!!     inputs is empty");
+            }
+            else
+            {
+                double x, eSum = 1, arcSum = 0, cosSum = 1, degX;
+                double pE = 1, fE = 1, pA = 1, fA = 1, pC = 1, fC = 1, sign = 1;
+                int n;
+                n = Convert.ToInt32(tbLength.Text);
+                x = Convert.ToDouble(tbX.Text);
+                degX = x * (3.142 / 180.0);
+                pE = x;
+                arcSum = degX;
+                for (int i = n - 1; i > 0; --i)
+                {
+                    eSum = 1 + x * eSum / i;
+                }
+                for (int i = 1; i <= n; i++)
+                {
+                    arcSum = arcSum + ((sign * pA) / fA);
+
+                    fA = 2 * i + 1;
+                    pA = pA * degX * degX;
+                    sign = sign * -1;
+                    fC = fC * (2 * i - 1) * (2 * i);
+                    pC = pC * degX * degX;
+                    cosSum = cosSum + sign * pC / fC;
+                }
+                lExp.Text = Convert.ToString(eSum);
+                lArctan.Text = Convert.ToString(arcSum);
+                lCos.Text = Convert.ToString(cosSum);
+            }
+        }
+    }
+}
